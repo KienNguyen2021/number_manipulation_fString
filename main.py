cc235ac7445d8521694367e30aea83db829ab624
kien1nguyen <>
2021-04-22 23:06:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print (8/3)         # 2.66666666666665
+print (int(8/3))    # 2
+print (round(8/3))  #  3
+print (round(8/3,2 )) # 2.67
+print ( round(2.6666666661, 2))  # 2.67
+
+#F -string
+score =0  #integer
+height = 1.8   #float
+isWinning = True  #boolean
+print(f"your score is {score},your height is {height}, you are winning is {isWinning}")
+
